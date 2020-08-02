7378fd4beb360b8c19d6c3474fa2e82ea62de8e4
Mytsu <tlc.jooker@gmail.com>
2020-08-02 16:30:39 -0300

    fix regex, refactoring, removing scripts from chapter.content, added file dir as argument

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ interface Chapter {
     content: string;
 }
 
-export default async (url: string): Promise<void> => {
+export default async (url: string, dir: string): Promise<void> => {
     try {
         const browser = await puppeteer.launch();
         const novelPage = await browser.newPage();
@@ -22,15 +22,15 @@ export default async (url: string): Promise<void> => {
 
         const novel = await novelPage.evaluate( () => {
             let title = document.querySelector('#info > h1')?.textContent;
-                title = !!title ? title : '';
+                title = title ? title : 'DEU';
             let author = document.querySelector('#info > p')?.textContent?.split('：')[1];
-                author = !!author ? author : '';
+                author = author ? author : 'MUITA';
 
-            let chapterLinks: string[] = [];
-            let query = document.querySelectorAll('#list > dl > dd > a')
+            const chapterLinks: string[] = [];
+            const query = document.querySelectorAll('#list > dl > dd > a')
             for (let index = 0; index < query.length; index++) {
                 const link = query[index].getAttribute('href');
-                chapterLinks.push( !!link ? link : '' );
+                chapterLinks.push( link ? link : 'MERDA' );
             }
 
             return { title, author, chapterLinks } as Novel;
@@ -38,45 +38,48 @@ export default async (url: string): Promise<void> => {
 
         console.log(`Fetching novel: ${novel.title}`);
 
-        let chapters: Chapter[] = [];
+        let data = 
+`---
+CJKmainfont: Noto Serif CJK TC
+---
+
+# ${novel.title}
+
+##### ${novel.author}
+
+`;
+
+        let chapter: Chapter;
         
         for (let index = 0; index < novel.chapterLinks.length; index++) {
             await chapterPage.goto(`${url}${novel.chapterLinks[index]}`).catch(e => console.log(e));
-            chapters.push(await chapterPage.evaluate( () => {
+            chapter = await chapterPage.evaluate( () => {
                 let title = document.querySelector('.bookname > h1')?.textContent;
-                    title = !!title ? title : '';
+                    title = title ? title : 'DEU';
                 let content = document.querySelector('#content')
                     ?.textContent
-                    ?.replace(/<br\s*[\/]?>/gi, "\n");
-                content = !!content ? content : '';
+                    ?.replace(/<\s*br\s*\/?>/gi, "\n");
+                content = content ? content : 'MERDA';
+
+                content.replace('(adsbygoogle = window.adsbygoogle || []).push({});', '');
+                content.replace('ChapterMid();', '');
+                content.replace('chaptererror();', '');
+
                 return { title, content } as Chapter;
-            }));
+            });
 
-            console.log(`Chapter fetched: ${chapters[index].title}`);
+            console.log(`Chapter fetched: ${chapter.title}`);
+            
+            data = data +
+`## ${chapter.title}
 
+${chapter.content}
+
+`;
         }
 
-        console.log(`Writing data to file ${novel.title}.md`)
-
-        let data = `---
-        CJKmainfont: Noto Serif CJK TC
-        ---
-        
-        # ${novel.title}
-        
-        ##### ${novel.author}
-        
-        `;
-
-        for (let index = 0; index < chapters.length; index++) {
-            data.concat(`## ${chapters[index].title}
-            
-            ${chapters[index].content}
-            
-            `);            
-        }
-
-        fs.writeFileSync(`${novel.title}.md`, data);
+        console.log(`Writing data to file ${dir}\\${novel.title}.md`)
+        fs.writeFileSync(`${dir}\\${novel.title}.md`, data);
 
         browser.close();
     } catch(err) { console.error(err); }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import yargs from 'yargs';
 import App from './app';
+import path from 'path'
 
-/*
 const options = yargs
- .usage(`Usage:  ${process.argv[1]} -u <url>`)
- .option('u',
-    {
-        alias: 'url',
-        describe: 'Full Novel URL, e.g. https://wuxiaworld.co/The-Legendary-Mechanic/',
-        type: 'string',
-        demandOption: true
+    .usage(`Usage:  ${process.argv[1]} -u <url>`)
+    .option("u", {
+        alias: "url",
+        describe:
+            "Full Novel URL, e.g. https://wuxiaworld.co/The-Legendary-Mechanic/",
+        type: "string",
+        demandOption: true,
     })
- .argv;
+    .option("d", {
+        alias: "directory",
+        describe: "Directory where to save the novel file",
+        type: "string",
+    }).argv;
 
-App(options.u);
-
-**/
+App(options.u, options.d ? options.d : path.resolve(__dirname));
 
 // TODO: setup yargs again
 
-App('https://www.wuxiaworld.co/Strongest-Abandoned-Son/');
+// App('https://www.wuxiaworld.co/Strongest-Abandoned-Son/');
