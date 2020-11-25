4a8157d5d9fcf53ae64c1ecc945ec6acfd9e174d
Mytsu <tlc.jooker@gmail.com>
2020-11-25 16:30:20 -0300

    update getNovelMetaData test

--- spec/app.spec.ts
+++ spec/app.spec.ts
@@ -1,23 +1,25 @@
 import 'mocha';
-import * as app from '../src';
 import { expect } from 'chai';
+import fs from 'fs';
+import path from 'path';
+import {
+    Novel,
+    getNovelMetadata
+} from '../src';
 
 describe('Function getNovelMetadata', () => {
 
-    const url = 'https://www.wuxiaworld.co/The-Legendary-Mechanic/';
-    const blank: app.Novel = {
-        title: '',
-        author: '',
-        chapters: [],
-        chapterLinks: []
-    }
-    let metadata: app.Novel;
+    let novel: Novel;
+    let data: string;
+
+    const authorName = 'Legion20';
 
     before(async () => {
-        metadata = await app.getNovelMetadata(url) || blank;
+        data = fs.readFileSync(path.resolve('./samples/Supreme_Magus.html')).toString('utf-8');
+        novel = await getNovelMetadata(data);
     })
 
-    it('Novel Author Should be \'Chocolion\'', () => {
-        expect(metadata?.author).to.be.equal('Chocolion');
+    it(`Author's name should be '${authorName}'`, () => {
+        expect(novel?.author).to.be.equal(authorName);
     })
 });
