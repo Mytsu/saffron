0e4aeb30a3b933e1fa606bada324b637cb850289
Mytsu <tlc.jooker@gmail.com>
2020-12-01 17:21:04 -0300

    refactoring main

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs';
-import { Novel, dumpToFile, getNovel } from './app';
-import path from 'path'
+import { dumpToFile, getNovel } from './app';
+import { Novel } from './types';
+import path from 'path';
 
 const options = yargs
     .usage(`Usage:  ${process.argv[1]} -u <url>`)
-    .option("u", {
-        alias: "url",
+    .option('u', {
+        alias: 'url',
         describe:
-            "Full Novel URL, e.g. https://wuxiaworld.co/The-Legendary-Mechanic/",
-        type: "string",
+            'Full Novel URL, e.g. https://wuxiaworld.co/The-Legendary-Mechanic/',
+        type: 'string',
         demandOption: true,
     })
-    .option("d", {
-        alias: "directory",
-        describe: "Directory where to save the novel file",
-        type: "string",
+    .option('d', {
+        alias: 'directory',
+        describe: 'Directory where to save the novel file',
+        type: 'string',
     }).argv;
 
 getNovel(options.u).then((novel: Novel) => {
-    dumpToFile(novel, options.d ? path.resolve(options.d) : path.resolve(__dirname));
+    dumpToFile(
+        novel,
+        options.d ? path.resolve(options.d) : path.resolve(__dirname)
+    );
 });
