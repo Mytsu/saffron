d1c13c92055cee8ef1eef20e300400dbf3ed0834
Mytsu <tlc.jooker@gmail.com>
2020-12-06 12:26:33 -0300

    added tests

--- spec/scrapper.spec.ts
+++ spec/scrapper.spec.ts
@@ -10,12 +10,28 @@ const testPool = [
     {
         domain: 'WuxiaWorld.co',
         url: 'https://www.wuxiaworld.co/Supreme-Magus/',
-        response: { title: 'Supreme Magus', author: 'Legion20' },
+        response: {
+            title: 'Supreme Magus',
+            author: 'Legion20',
+            coverUrl:
+                'https://img.wuxiaworld.co/BookFiles/BookImages/Supreme-Magus.jpg',
+            // Specific domain formats links without the domain
+            first_chapterLink:
+                '/Supreme-Magus/2114127.html',
+        },
     },
     {
         domain: 'ReadLightNovel.org',
         url: 'https://www.readlightnovel.org/supreme-magus',
-        response: { title: 'Supreme Magus', author: '' },
+        // RLN doesn't provide correct metadata for most novels
+        response: {
+            title: 'Supreme Magus',
+            author: 'N/A',
+            coverUrl:
+                'https://www.readlightnovel.org/uploads/posters/1554422569.jpg',
+            first_chapterLink:
+                'https://www.readlightnovel.org/supreme-magus/chapter-prologue',
+        },
     },
 ];
 
@@ -37,20 +53,36 @@ describe('Supported Domains', () => {
 
 testPool.forEach((test) => {
     describe(test.domain, () => {
-    
         describe('getNovelMetadata', () => {
-    
             before(async () => {
                 const { data } = await axios.get(test.url);
                 metadata = await getScrapper(test.url).getNovelMetadata(data);
             });
-    
-            it(`Novel's name should be '${test.response.title}`, () => {
+
+            it(`Novel's name should be '${test.response.title}'`, () => {
                 expect(metadata?.title).to.be.equal(test.response.title);
             });
-    
+
             it(`Author's name should be '${test.response.author}'`, () => {
                 expect(metadata?.author).to.be.equal(test.response.author);
+            });
+
+            if (test.response.coverUrl) {
+                it(`Should return the cover image url`, () => {
+                    expect(metadata.coverUrl).to.be.equal(
+                        test.response.coverUrl
+                    );
+                });
+            }
+
+            it(`Should have chapter links`, () => {
+                expect(metadata?.chapterLinks).to.have.length.above(0);
+            });
+
+            it(`Should have first chapter link matching sample`, () => {
+                expect(metadata?.chapterLinks[0]).to.be.equal(
+                    test.response.first_chapterLink
+                );
             });
         });
     });
