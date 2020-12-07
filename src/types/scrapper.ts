bbe7368dfedb5e666518859d84a2ef6df5257bdf
Mytsu <tlc.jooker@gmail.com>
2020-12-07 10:53:38 -0300

    set url as Scrapper variable

--- src/types/scrapper.ts
+++ src/types/scrapper.ts
@@ -1,7 +1,9 @@
 import { NovelMetadata, Chapter, Novel } from './novel';
 
 export interface Scrapper {
+    url: string;
     getNovel(): Promise<Novel>;
     getNovelMetadata(data: string): Promise<NovelMetadata>;
     getChapter(url: string): Promise<Chapter>;
+    // getChapters(init: number, amount: number): Promise<Novel>;
 }
