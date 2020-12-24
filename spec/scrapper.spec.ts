3602018d4c4c2aaf350e7cc601d04a4007312dee
Mytsu <tlc.jooker@gmail.com>
2020-12-24 19:14:53 -0300

    update spec

--- spec/scrapper.spec.ts
+++ spec/scrapper.spec.ts
@@ -3,38 +3,10 @@ import { expect } from 'chai';
 import axios from 'axios';
 import { NovelMetadata, Chapter } from '../src/types';
 import { getScrapper, getSupportedDomains } from './../src/app';
+import * as responses from './responses.json'
 
 let metadata: NovelMetadata;
-
-const testPool = [
-    {
-        domain: 'WuxiaWorld.co',
-        url: 'https://www.wuxiaworld.co/Supreme-Magus/',
-        response: {
-            title: 'Supreme Magus',
-            author: 'Legion20',
-            coverUrl:
-                'https://img.wuxiaworld.co/BookFiles/BookImages/Supreme-Magus.jpg',
-            // Specific domain formats links without the domain
-            chapterLinks: ['/Supreme-Magus/2114127.html'],
-        },
-    },
-    {
-        domain: 'ReadLightNovel.org',
-        url: 'https://www.readlightnovel.org/supreme-magus',
-        // RLN doesn't provide correct metadata for most novels
-        response: {
-            title: 'Supreme Magus',
-            author: 'N/A',
-            coverUrl:
-                'https://www.readlightnovel.org/uploads/posters/1554422569.jpg',
-            chapterLinks: [
-                'https://www.readlightnovel.org/supreme-magus/chapter-prologue',
-                'https://www.readlightnovel.org/supreme-magus/chapter-917',
-            ],
-        },
-    },
-];
+const testPool = responses.testPool;
 
 describe('Supported Domains', () => {
     let domains: string[];
