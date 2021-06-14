719979391c587fa68dff0922f21eecbf509538cb
Mytsu <tlc.jooker@gmail.com>
2021-06-14 17:41:14 -0300

    removing test.mjs

--- test.mjs
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-axios.get('https://ranobes.net/novels/534520-cultivation-online-v162128.html', {
-    headers: { 'User-Agent': 'Mozilla' }
-}).then((response) => { console.log(response ) });
