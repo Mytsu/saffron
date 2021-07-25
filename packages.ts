04ff15600d01b71692f69f8a765e92911ceca348
Mytsu <tlc.jooker@gmail.com>
2021-07-25 13:38:01 -0300

    update std, remove denodb export

--- packages.ts
+++ packages.ts
@@ -1,12 +1,7 @@
 export {
-  DataTypes,
-  Model,
-  Relationships,
-} from 'https://deno.land/x/denodb@v1.0.38/mod.ts';
-export {
   DOMParser,
   HTMLDocument,
 } from 'https://deno.land/x/deno_dom@v0.1.12-alpha/deno-dom-wasm.ts';
 export { config } from 'https://deno.land/x/dotenv@v2.0.0/mod.ts';
-export { parse } from 'https://deno.land/std@0.100.0/flags/mod.ts';
+export { parse } from 'https://deno.land/std@0.102.0/flags/mod.ts';
 export { encodeUrl } from 'https://deno.land/x/encodeurl@1.0.0/mod.ts';
