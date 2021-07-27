aeffd6e435543e5c70f3c32dd001e4c435ec1706
Mytsu <tlc.jooker@gmail.com>
2021-07-27 18:59:05 -0300

    bump std to 0.103.0

--- tests/packages.ts
+++ tests/packages.ts
@@ -2,6 +2,6 @@ export {
   assert,
   assertEquals,
   assertStringIncludes,
-} from 'https://deno.land/std@0.100.0/testing/asserts.ts';
-export { exists } from 'https://deno.land/std@0.100.0/fs/mod.ts';
-export { delay } from 'https://deno.land/std@0.100.0/async/delay.ts';
+} from 'https://deno.land/std@0.103.0/testing/asserts.ts';
+export { exists } from 'https://deno.land/std@0.103.0/fs/mod.ts';
+export { delay } from 'https://deno.land/std@0.103.0/async/delay.ts';
